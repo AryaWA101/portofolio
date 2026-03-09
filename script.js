99704411ee449edda8cbc00a1e1194d1b3028a3e
AryaWA101 <aryawibawa1502@gmail.com>
2026-03-09 18:11:06 +0700

    portofolio1

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnSapa = document.getElementById('btn-sapa');
+
+btnSapa.addEventListener('click', function() {
+    alert('Terima kasih sudah mengunjungi portofolio saya! Silakan kirimkan email untuk berkolaborasi.');
+});
